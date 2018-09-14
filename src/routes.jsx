53b8154c002b7a2795b6dd37d3abf87a1dd97ec8
Jusley Arley <jusleyarley@hotmail.com>
2018-09-13 23:44:52 -0300

    Configurações iniciais da página: Router, divisão de pastas, etc

---
+++ src/routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+
+//Components
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+//Views
+// import Home from './views/Home';
+
+const Main = () => (
+  <Router>
+    <main>
+      {/* <Header /> */}
+      <Switch>
+        {/* <Route exact path='/' component={Home} /> */}
+      </Switch>
+      {/* <Footer /> */}
+    </main>
+  </Router>
+);
+
+export default Main;
